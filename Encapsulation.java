0053504cb127849fb96822baec7efef0c2d3ac83
Gopinath-M0612 <gopimano1997@gmail.com>
2025-09-30 09:48:17 +0530

    Add files via upload
    Task 30/09/25

---
+++ Encapsulation.java
@@ -0,0 +1,94 @@
+package sla.course;
+
+public class Encapsulation {
+	
+	private String soap;
+	private boolean availabilityOfSoap = true;
+	private int priceOfSoap = 40;
+	private String toothpaste;
+	private boolean avilabilityOfPaste = true;
+	private int priceOfpaste = 30;
+	private String shampoo;
+	private boolean avilabilityOfShampoo = false;
+	private int priceOfShampoo = 60;
+	private String brush;
+	private boolean availabilityOfBrush = true;
+	private int priceOfBrush = 35;
+	
+	
+	public String getSoap() {
+		return soap;
+	}
+	public void setSoap(String soap) {
+		this.soap = soap;
+	}
+	public boolean isAvailabilityOfSoap() {
+		return availabilityOfSoap;
+	}
+	public void setAvailabilityOfSoap(boolean availabilityOfSoap) {
+		this.availabilityOfSoap = availabilityOfSoap;
+	}
+	public int getPriceOfSoap() {
+		return priceOfSoap;
+	}
+	public void setPriceOfSoap(int priceOfSoap) {
+		this.priceOfSoap = priceOfSoap;
+	}
+	public String getToothpaste() {
+		return toothpaste;
+	}
+	public void setToothpaste(String toothpaste) {
+		this.toothpaste = toothpaste;
+	}
+	public boolean isAvilabilityOfPaste() {
+		return avilabilityOfPaste;
+	}
+	public void setAvilabilityOfPaste(boolean avilabilityOfPaste) {
+		this.avilabilityOfPaste = avilabilityOfPaste;
+	}
+	public int getPriceOfpaste() {
+		return priceOfpaste;
+	}
+	public void setPriceOfpaste(int priceOfpaste) {
+		this.priceOfpaste = priceOfpaste;
+	}
+	public String getShampoo() {
+		return shampoo;
+	}
+	public void setShampoo(String shampoo) {
+		this.shampoo = shampoo;
+	}
+	public boolean isAvilabilityOfShampoo() {
+		return avilabilityOfShampoo;
+	}
+	public void setAvilabilityOfShampoo(boolean avilabilityOfShampoo) {
+		this.avilabilityOfShampoo = avilabilityOfShampoo;
+	}
+	public int getPriceOfShampoo() {
+		return priceOfShampoo;
+	}
+	public void setPriceOfShampoo(int priceOfShampoo) {
+		this.priceOfShampoo = priceOfShampoo;
+	}
+	public String getBrush() {
+		return brush;
+	}
+	public void setBrush(String brush) {
+		this.brush = brush;
+	}
+	public boolean isAvailabilityOfBrush() {
+		return availabilityOfBrush;
+	}
+	public void setAvailabilityOfBrush(boolean availabilityOfBrush) {
+		this.availabilityOfBrush = availabilityOfBrush;
+	}
+	public int getPriceOfBrush() {
+		return priceOfBrush;
+	}
+	public void setPriceOfBrush(int priceOfBrush) {
+		this.priceOfBrush = priceOfBrush;
+	}
+	
+	
+
+}
